5896d4e5f42ee3c6fa72766db8f4ac5efa4a9577
Pranav <6195660+0Pranav@users.noreply.github.com>
2023-07-07 13:59:05 -0400

    feat: add nvim rc[wip]

---
+++ nvim/.config/nvim/lua/pranav/init.lua
@@ -0,0 +1,28 @@
+require("pranav.remap")
+require("pranav.set")
+
+require("gruvbox").setup({
+  undercurl = false,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = "soft", -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = true,
+  transparent_mode = true,
+})
+
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
