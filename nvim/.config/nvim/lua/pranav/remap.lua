c2ad931bb5b5630ad8b7a59c02f2f3727af23fcb
Pranav <6195660+0Pranav@users.noreply.github.com>
2023-07-11 02:42:01 -0400

    fix: add oil mapping

--- nvim/.config/nvim/lua/pranav/remap.lua
+++ nvim/.config/nvim/lua/pranav/remap.lua
@@ -1,6 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
+vim.keymap.set("n", "<leader>pv", require("oil").open, { desc = "Open parent directory" })
 -- make sure the cursor stays in the middle of the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
